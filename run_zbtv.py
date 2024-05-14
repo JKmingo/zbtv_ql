ab9d07a9017db7de0fd3856e43d370a9565a7929
JKmingo <jimic0831@gmail.com>
2024-05-14 15:05:36 +0800

    Update run_zbtv.py

--- run_zbtv.py
+++ run_zbtv.py
@@ -14,8 +14,6 @@ if __name__ == '__main__':
     repo_url = "https://github.com/JKmingo/ZBTV.git"
     # 本地仓库目录
     base_path = os.getcwd()
-    repo_dir = "."
-    repo_path = os.path.join(base_path, repo_dir)
-    subprocess.run(["git", "clone", repo_url])
+    subprocess.run(["git", "clone", base_path])
     exec(open("main.py").read())
     
